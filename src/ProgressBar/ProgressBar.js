368cde5c150d2cd34ff9cc54c30b551e6e54550d
Rachit Garg <rachitgarg56@gmail.com>
2024-03-14 15:50:46 +0530

    initial commit

---
+++ src/ProgressBar/ProgressBar.js
@@ -0,0 +1,32 @@
+
+import { useEffect, useState } from 'react';
+import './ProgressBar.css';
+
+const ProgressBar = (props) => {
+
+    const [percentage, SetPercentage] = useState(0);
+    
+    useEffect(()=> {
+
+        setTimeout(() => {
+
+            SetPercentage(percentage+1);
+
+            if (percentage === 99) {
+                props.handleMessage();
+            }
+
+        }, 200);
+
+    },[(percentage < 99) ? percentage : null])
+
+    return (
+        <div className='progress-bar'>
+            <div className='progress'>
+                <span>{percentage + "%"}</span>
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar;
